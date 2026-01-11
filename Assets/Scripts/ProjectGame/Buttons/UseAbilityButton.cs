40d402c8ab566c53b0bd7aadf40f4f509b91c8fb
Artyom Bakhterev <delta38tema@gmail.com>
2026-01-11 14:51:43 +0500

    Added method to setup ICharacterAbility beneath inspector through another script.

--- Assets/Scripts/ProjectGame/Buttons/UseAbilityButton.cs
+++ Assets/Scripts/ProjectGame/Buttons/UseAbilityButton.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class UseAbilityButton: MenuButtonLeftClickBase
 {
     [SerializeField] private InterfaceReference<ICharacterAbility> _abilityLogic;
+
+    public void SetupAbilityButton(ICharacterAbility ability)
+    {
+        _abilityLogic.Value = ability;
+    }
     
     protected override void FireEvent()
     {
